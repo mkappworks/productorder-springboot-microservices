d4e8263ab3c560d93e85f2720040b14d81a7c2c3
Malith Kuruppu <info.mkappworks@gmail.com>
2023-09-26 06:43:45 +0200

    Adding OrderService

---
+++ order-service/src/main/java/com/mkappworks/orderservice/controller/OrderController.java
@@ -0,0 +1,24 @@
+package com.mkappworks.orderservice.controller;
+
+import com.mkappworks.orderservice.dto.OrderRequest;
+import com.mkappworks.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        orderService.placeOrder(orderRequest);
+        return "Order Placed Successfully";
+    }
+}
+
+
---
+++ order-service/src/main/java/com/mkappworks/orderservice/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.mkappworks.orderservice.repository;
+
+import com.mkappworks.orderservice.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
